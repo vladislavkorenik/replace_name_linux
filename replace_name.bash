593ca56201249e09a4f0e252b191c26a9ec99ef4
Vladislav Korenik <vladislav.korenik@codex-soft.com>
2022-11-26 05:13:28 -0500

    Improve grep path and loop code

--- replace_name.bash
+++ replace_name.bash
@@ -6,19 +6,8 @@ oldNamesArray=($oldNamesString)
 newNamesString=$(cat  ./fontawesome-names/new.txt |tr "\n" " ")
 newNamesArray=($newNamesString)
 
-fileNameWithOldIconPaths='output.txt'
 
 for (( i=0; i<=${#oldNamesArray[@]} - 1; i++ ))
 do
-  grep -E -w -Rl --exclude-dir={node_modules,_deprecated,assets,fontawesome} fa-${oldNamesArray[i]} ~/Public/Macmillan/*/frontend ~/Public/Macmillan/*/src > output.txt
-  wait
-  
-  for path in `cat $fileNameWithOldIconPaths`; do
-    sed -i "s/${oldNamesArray[i]}/${newNamesArray[i]}/g" $path
-    wait
-  done
-  wait
+  grep -E -w -Rl --exclude-dir={node_modules,_deprecated,assets,fontawesome} fa-${oldNamesArray[i]} ~/Public/Macmillan/*/{src,frontend} | xargs sed -i "s/${oldNamesArray[i]}/${newNamesArray[i]}/g"
 done
-
-
-
